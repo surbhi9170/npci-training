2caff7048e1a962b52f7411c7ea2ced1a40978f4
Surbhi jain <bgpsurbhi@gmail.com>
2021-09-17 10:50:02 +0530

    assignment2

--- main.cpp
+++ main.cpp
@@ -3,14 +3,36 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 
-int main(int argc, char** argv) {
-	
+
+int position( )
+{
+	int a[5]={1,4,5,9,10};
+	int num=0;
+	cin>>num;
+	for (int i=0;i<5;i++)
+   {
+       cout<<a[i]<<" ";
+   }
+	for(int i=0;i<5;i++)
+	{
+		if(a[i]==num)
+		{
+			cout<<"position is "<<i+1<<"\n";
+		}
+	}
+	return 0;
+}
+
+int ascending()
+{
+	cout<<"Ascending order for";
 	int i,j,temp=0;
    int arr[5]={1,5,10,8,9};
    for (int i=0;i<5;i++)
    {
        cout<<arr[i]<<" ";
    }
+   cout<<" is :";
    for(i=0;i<5;i++)
 	{		
 		for(j=i+1;j<5;j++)
@@ -28,5 +50,11 @@ int main(int argc, char** argv) {
    {
        cout<<arr[i]<<" ";
    }
+   return 0;
+}
+int main(int argc, char** argv) {
+	
+	position();
+	ascending();
 	return 0;
 }
