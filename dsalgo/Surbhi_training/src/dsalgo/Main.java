d24f7b92f3f20928e0905a326c4c5cd26d019094
surbhi9170 <bgpsurbhi@gmail.com>
2021-09-17 18:52:30 +0530

    circular queue

---
+++ dsalgo/Surbhi_training/src/dsalgo/Main.java
@@ -0,0 +1,20 @@
+package dsalgo;
+
+
+public class Main {
+	public static void main(String[] args) {
+
+
+		CircualQueue q = new CircualQueue(5);
+
+	   
+
+	    q.enQueue(1);
+	    q.enQueue(2);
+	    q.enQueue(3);
+	    q.enQueue(4);
+	    q.enQueue(5);
+
+    }
+
+}
