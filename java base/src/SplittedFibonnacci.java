43d8aaab9514cc014f4fbfd9f58d752bb614fbf3
Surbhi jain <bgpsurbhi@gmail.com>
2021-09-22 13:27:57 +0530

    Assignment for Splitted Fibonnaci in java basics

--- java base/src/SplittedFibonnacci.java
+++ java base/src/SplittedFibonnacci.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class SplittedFibonnacci {
-}
+import java.util.*;
+public class SplittedFibonnacci {
+
+    public static int callme(int l)
+    {
+
+            if (l == 0)
+                return 0;
+            else if (l == 1)
+                return 1;
+            else {
+                return callme(l - 1) + callme(l - 2);
+            }
+
+
+    }
+    public static int callme2(int l)
+    {
+
+            if (l == 0)
+                return 0;
+            else if (l == 1)
+                return 1;
+            else {
+                return callme(l - 1) + callme(l - 2);
+            }
+
+
+    }
+
+        public static void main(String[] args)
+        {
+            for(int i=0;i<5;i++)
+            {
+
+                if(i%2==0)
+                    System.out.print(callme(i)+",");
+                else
+                    System.out.print(callme2(i)+",");
+
+            }
+
+        }
+    }
+
