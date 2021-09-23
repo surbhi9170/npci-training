48bdbc32759f9016e690f86152d75067f771f623
Surbhi jain <bgpsurbhi@gmail.com>
2021-09-23 15:39:52 +0530

    String immutable example java oops

---
+++ java base/src/Strings/Main.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Strings
+        String s1="hello";
+        String s2="hello";
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        s2="surbhi";
+        System.out.println(s2.hashCode());
+
+
+    }
+}
+
